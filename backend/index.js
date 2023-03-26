ee1a5527ef680aaca99edcba69e8e5fa1e24a64a
ABHISHEKyadav281 <yyadavabhishek9@gmail.com>
2023-03-26 13:37:29 +0530

    url encoded

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,7 @@ app.use(
     })
 );
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use('/api', require('./Routes/CreateUsers'));
 app.use('/api', require('./Routes/DisplayData'));
