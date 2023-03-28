9f79aee31ad49120b6ffca29f0ec48df70eec802
ABHISHEKyadav281 <yyadavabhishek9@gmail.com>
2023-03-28 12:05:33 +0530

    usermodel activated

--- backend/Routes/DisplayData.js
+++ backend/Routes/DisplayData.js
@@ -6,8 +6,8 @@ const food_items=require("../models/food_items");
 
 router.get('/fooddata',async (req,res)=>{
     try{
-        // const data = await food_items.find();
-        const data =await mongoose.connection.db.collection("food_items").find().toArray();
+        const data = await food_items.find();
+        // const data =await mongoose.connection.db.collection("food_items").find().toArray();
         const data1 =await mongoose.connection.db.collection("food_category").find().toArray();
         console.log("all set");
         console.log(data);
