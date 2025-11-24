d386df651dd51f4059550fe9ba8876c66646f624
Abhishek Yadav <98586090+ABHISHEKyadav281@users.noreply.github.com>
2025-11-25 01:51:19 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 5000;
-const cookieParser = require("cookie-parser")
-const mongoDB = require('./db')
+const port = process.env.PORT || 5000;   // <<< REQUIRED FOR RENDER
+const cookieParser = require("cookie-parser");
+const mongoDB = require('./db');
+
 mongoDB();
 
 app.get('/', (req, res) => {
     res.send("hello world");
-})
+});
 
-// app.use((req,res,next)=>{
-//     res.setHeader("Access-Control-Allow-Origin",["https://taste-swings.vercel.app/","http://localhost:3000"]);
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Oringin,X-Requested-With,Content-Type,Accept",
-//     );
-//     next(); 
-// })
 app.use(
     require("cors")({
         origin: [
@@ -26,12 +19,57 @@ app.use(
         credentials: true,
     })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
+
 app.use('/api', require('./Routes/CreateUsers'));
 app.use('/api', require('./Routes/DisplayData'));
 app.use('/api', require('./Routes/OrderData'));
+
 app.listen(port, () => {
-    console.log("listening");
+    console.log("Server listening on port", port);
 });
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+// const port = 5000;
+// const cookieParser = require("cookie-parser")
+// const mongoDB = require('./db')
+// mongoDB();
+
+// app.get('/', (req, res) => {
+//     res.send("hello world");
+// })
+
+// // app.use((req,res,next)=>{
+// //     res.setHeader("Access-Control-Allow-Origin",["https://taste-swings.vercel.app/","http://localhost:3000"]);
+// //     res.header(
+// //         "Access-Control-Allow-Headers",
+// //         "Oringin,X-Requested-With,Content-Type,Accept",
+// //     );
+// //     next(); 
+// // })
+// app.use(
+//     require("cors")({
+//         origin: [
+//             "https://taste-swings.vercel.app",
+//             "http://localhost:3000",
+//         ],
+//         credentials: true,
+//     })
+// );
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser())
+// app.use('/api', require('./Routes/CreateUsers'));
+// app.use('/api', require('./Routes/DisplayData'));
+// app.use('/api', require('./Routes/OrderData'));
+// app.listen(port, () => {
+//     console.log("listening");
+// });
