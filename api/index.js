010bf3f595bb9e298a1ec6d375b098a39684f9d9
Abhishek Yadav <98586090+ABHISHEKyadav281@users.noreply.github.com>
2025-11-25 02:55:09 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;   // <<< REQUIRED FOR RENDER
+const port = process.env.PORT || 5000;
 const cookieParser = require("cookie-parser");
 const mongoDB = require('./db');
 
 mongoDB();
 
-app.get('/', (req, res) => {
-    res.send("hello world");
-});
-
 app.use(
     require("cors")({
         origin: [
-            "https://taste-swings.vercel.app",
-            "http://localhost:3000",
+            "https://tasteswings.onrender.com",
+            "http://localhost:3000",              
         ],
         credentials: true,
     })
@@ -24,6 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
+
+// Routes
 app.use('/api', require('./Routes/CreateUsers'));
 app.use('/api', require('./Routes/DisplayData'));
 app.use('/api', require('./Routes/OrderData'));
@@ -31,45 +32,3 @@ app.use('/api', require('./Routes/OrderData'));
 app.listen(port, () => {
     console.log("Server listening on port", port);
 });
-
-
-
-
-
-// const express = require('express');
-// const app = express();
-// const port = 5000;
-// const cookieParser = require("cookie-parser")
-// const mongoDB = require('./db')
-// mongoDB();
-
-// app.get('/', (req, res) => {
-//     res.send("hello world");
-// })
-
-// // app.use((req,res,next)=>{
-// //     res.setHeader("Access-Control-Allow-Origin",["https://taste-swings.vercel.app/","http://localhost:3000"]);
-// //     res.header(
-// //         "Access-Control-Allow-Headers",
-// //         "Oringin,X-Requested-With,Content-Type,Accept",
-// //     );
-// //     next(); 
-// // })
-// app.use(
-//     require("cors")({
-//         origin: [
-//             "https://taste-swings.vercel.app",
-//             "http://localhost:3000",
-//         ],
-//         credentials: true,
-//     })
-// );
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser())
-// app.use('/api', require('./Routes/CreateUsers'));
-// app.use('/api', require('./Routes/DisplayData'));
-// app.use('/api', require('./Routes/OrderData'));
-// app.listen(port, () => {
-//     console.log("listening");
-// });
